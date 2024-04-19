6d030cf0cc16aea64202a7c3ee9adc452a438e21
d.akinfiev <d.akinfiev@oneliya.ru>
2024-04-19 14:01:48 +0300

    added negative tests, moved functions fcrom main_page to base_page, added tests for login link

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,8 @@ class ProductPageLocators:
     PRODUCT_NAME = (By.CSS_SELECTOR, ".product_main h1")
     ALERT_PRODUCT_NAME = (By.CSS_SELECTOR, ".alertinner > strong")
     ALERT_PRODUCT_PRICE = (By.CSS_SELECTOR, ".alertinner p strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success")
+
+class BasePageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- pages/product_page.py
+++ pages/product_page.py
@@ -18,3 +18,11 @@ class ProductPage(BasePage):
         product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
         alert_product_price = self.browser.find_element(*ProductPageLocators.ALERT_PRODUCT_PRICE).text
         assert product_price == alert_product_price, "product price in cart is not the same as on page"
+
+    def should_not_be_success_message(self):
+        assert self.element_is_not_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def element_is_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
